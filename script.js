0d0d7e3d17e76feea36d498b5e1712245e777544
pedroLopes-a22104722 <pedrosilvestrelopes@hotmail.com>
2023-04-23 01:24:53 +0100

    tuso acabado, so falta mesmo o butao de dark mode

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ toggle.addEventListener('click', function(){
     this.classList.toggle()
 });
 
-
+document.getElementById('actual_date').innerHTML = Date();
 
 var temp = 0;
+
+
 
 function insert(input){
     var number = document.getElementById('result').innerHTML;
@@ -35,3 +37,32 @@ function backSpace(){
     var expression = document.getElementById('result').innerHTML;
     document.getElementById('result').innerHTML = expression.substring(0, expression.length-1);
 }
+
+
+
+document.getElementById('benficaButtom').onclick = function(){
+
+    document.getElementById('phraseOverImage').innerHTML = document.getElementById('textBox').value;
+
+    document.getElementById('textBox').value = "";
+
+
+}
+
+
+document.getElementById('nameButtom').onclick = function(){
+
+ 
+    document.getElementById('inputedName1').innerHTML = document.getElementById('nameTextBox').value;
+    document.getElementById('inputedName2').innerHTML = document.getElementById('nameTextBox').value;
+    document.getElementById('inputedName3').innerHTML = document.getElementById('nameTextBox').value;
+    document.getElementById('inputedName4').innerHTML = document.getElementById('nameTextBox').value;
+
+
+    document.getElementById('nameTextBox').value = "";
+
+
+}
+
+
+
