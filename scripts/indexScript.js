d7f2255b13cc7a6b25e7fb44c86b57e21156b869
pedroLopes-a22104722 <pedrosilvestrelopes@hotmail.com>
2023-04-30 16:35:38 +0100

    ACABADO

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,7 +1,29 @@
-const hdr = document.querySelector('hdr');
-const toggle = document.getElementById('toggle');
+const toggle = document.getElementById('toggleDark');
 
-function toggleDarkMode(){
-    toggle.classList.toggle('active');
-    hdr.classList.toggle('active');
+function change(){
+    var get = document.querySelector(".hdr");
+    get.classList.toggle("hdrDarlk");
+
 }
+
+function changeMyContainer(){
+    var get = document.querySelector(".my_container");
+    get.classList.toggle("my_containerDark");
+
+    get = document.querySelector(".name");
+    get.classList.toggle("nameDark");   
+}
+
+function changeAboutMeContainer(){
+    var get = document.querySelector(".about_me_container");
+    get.classList.toggle("about_me_container_dark");
+}
+
+// // var getMyContainer = document.querySelector(".my_container");
+// // get.classList.toggle("my_containerDarck")
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    change();
+    changeMyContainer();
+    changeAboutMeContainer()
+});
